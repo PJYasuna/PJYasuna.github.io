594890ae4e06c4482dca36f36c1457e6bfc38c57
pangjiayang <j4pang@ucsd.edu>
2025-08-26 23:14:34 -0700

    Modify the configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://PJYasuna.github.io',
-	base: 'joe-site',
 	integrations: [mdx(), sitemap()],
 });
